cb3ae12ac8766014d78e6aa2921e9c4d8f7dc9df
Marcus Vinicus <mvbrazz@hotmail.com>
2021-10-19 19:20:05 -0300

    Ultimo commit

--- test/utils.test.js
+++ test/utils.test.js
@@ -3,9 +3,36 @@ const { gerarNumeroAleatorio, acharCaracter } = require('../lib/utils');
 describe('Utils', () => {
     describe('gerarNumeroAleatorio', () => {
         test('fim nao pode ser negativo', () => {
-            expect(gerarNumeroAleatorio(20, -5))
-                .toBe(-1);
+
+            const aux = gerarNumeroAleatorio(1, 5);
+
+            expect(aux.correto).toBe(1);
         });
+ 
+        test('inicio nao pode ser negativo', () => {
+            const aux = gerarNumeroAleatorio(20, -5);
+
+            expect(aux.correto).toBe(-1);
+        });
+    
+        test('inicio maior ou igual ao fim', () => {
+            const aux = gerarNumeroAleatorio(20, 5);
+
+            expect(aux.correto).toBe(-2);
+        });
+        
+        test('O numero não é inteiro', () => {
+            const aux = gerarNumeroAleatorio(4.5, 5);
+
+            expect(aux.correto).toBe(-3);
+        });
+
+        test('Tudo Ok', () => {
+            const aux = gerarNumeroAleatorio(1, 15);
+
+            expect(aux.correto).toBe(1);
+        });
+       
     });
 
     describe('acharCaracter', () => {
@@ -13,5 +40,19 @@ describe('Utils', () => {
             expect(acharCaracter(-4, 'abcde', 'c'))
                 .toBe('comprimento invalido');
         });
+
+        test('comprimento diferente do real', () => {
+            expect(acharCaracter(4, 'abcde', 'c'))
+                .toBe('comprimento fornecido diferente do comprimento real');
+        });
+
+        test('Tudo ok', () => {
+            const aux = acharCaracter(4, 'abcd', 'c');
+
+            expect(aux.encontrado).toBe("caracter encontrado");
+            expect(aux.localizacao).toBe(2);
+        });
+
+
     });    
 });
